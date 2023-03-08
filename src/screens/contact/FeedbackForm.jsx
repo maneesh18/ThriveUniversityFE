e5b487121e486a262f96a0940006606b577a442a
Maneesh18 <maneeshreddy2468@gmail.com>
2023-03-08 14:05:41 +0530

    responsive changes for contact page

--- src/screens/contact/FeedbackForm.jsx
+++ src/screens/contact/FeedbackForm.jsx
@@ -1,37 +1,96 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function FeedbackForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Name: ${name}, Email: ${email}, Subject: ${subject}, Message: ${message}`);
-    setName('');
-    setEmail('');
-    setSubject('');
-    setMessage('');
+    console.log(formData);
+    setFormData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
   };
 
   return (
     <form className="feedback-form" onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" value={name} onChange={(event) => setName(event.target.value)} required />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={(event) =>
+            setFormData((prev) => {
+              return {
+                ...prev,
+                name: event.target.value,
+              };
+            })
+          }
+          required
+        />
       </div>
       <div className="form-group">
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={(event) =>
+            setFormData((prev) => {
+              return {
+                ...prev,
+                email: event.target.value,
+              };
+            })
+          }
+          required
+        />
       </div>
       <div className="form-group">
         <label htmlFor="subject">Subject:</label>
-        <input type="text" id="subject" name="subject" value={subject} onChange={(event) => setSubject(event.target.value)} required />
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          value={formData.subject}
+          onChange={(event) =>
+            setFormData((prev) => {
+              return {
+                ...prev,
+                subject: event.target.value,
+              };
+            })
+          }
+          required
+        />
       </div>
       <div className="form-group">
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" rows="5" value={message} onChange={(event) => setMessage(event.target.value)} required></textarea>
+        <textarea
+          id="message"
+          name="message"
+          rows="5"
+          value={formData.message}
+          onChange={(event) =>
+            setFormData((prev) => {
+              return {
+                ...prev,
+                message: event.target.value,
+              };
+            })
+          }
+          required
+        ></textarea>
       </div>
       <button type="submit">Submit</button>
     </form>
